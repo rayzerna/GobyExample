8b2e4a58c6ac255f05d0a8d6947beaa007396c0f
Ray.Sun <ray.sun@gaodun.com>
2021-02-19 10:01:16 +0800

    update to exited

---
+++ p071_context.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func hello1(w http.ResponseWriter, req *http.Request) {
+
+	ctx := req.Context()
+	fmt.Println("server: hello handler started")
+	defer fmt.Println("server: hello handler ended")
+
+	select {
+	case <-time.After(10 * time.Second):
+		_, _ = fmt.Fprintf(w, "hello\n")
+	case <-ctx.Done():
+
+		err := ctx.Err()
+		fmt.Println("server:", err)
+		internalError := http.StatusInternalServerError
+		http.Error(w, err.Error(), internalError)
+	}
+}
+
+func main() {
+	http.HandleFunc("/hello", hello1)
+	_ = http.ListenAndServe(":8090", nil)
+}
